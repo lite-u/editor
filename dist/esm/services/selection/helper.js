a81f568fee2cbe6958b1d5250178a1a42946f3e2
yahone <yahone.chow@gmail.com>
2025-05-11 19:54:11 +0900

    ###

---
+++ dist/esm/services/selection/helper.js
@@ -0,0 +1,72 @@
+import typeCheck from '../../core/typeCheck.js';
+export function modifySelected(idSet, action) {
+    if (typeCheck(idSet) !== 'set')
+        return;
+    let eventCallBackData = null;
+    if (idSet.size === 1) {
+        const first = [...idSet.values()][0];
+        if (this.elementManager.all.has(first)) {
+            // @ts-ignore
+            eventCallBackData = this.elementManager.all.get(first).toMinimalJSON();
+            // console.log(eventCallBackData)
+        }
+    }
+    const realSelectedModules = this.selection.getSelected;
+    this.selectedElementIDSet.clear();
+    if (action === 'replace') {
+        realSelectedModules.clear();
+    }
+    idSet.forEach((id) => {
+        switch (action) {
+            case 'add':
+                realSelectedModules.add(id);
+                break;
+            case 'delete':
+                realSelectedModules.delete(id);
+                break;
+            case 'toggle':
+                if (realSelectedModules.has(id)) {
+                    realSelectedModules.delete(id);
+                }
+                else {
+                    realSelectedModules.add(id);
+                }
+                break;
+            case 'replace':
+                realSelectedModules.add(id);
+                break;
+        }
+    });
+    realSelectedModules.forEach((id) => this.selectedElementIDSet.add(id));
+    // this.events.onSelectionUpdated?.(idSet, eventCallBackData)
+}
+export function updateSelectionCanvasRenderData() {
+    // const moduleProps = this.selection.getSelectedPropsIfUnique
+    return;
+    /*
+      if (moduleProps) {
+        const module = this.elementManager.all.get(moduleProps.id)
+        const {scale, dpr} = this.viewport
+        const lineWidth = 1 / scale * dpr
+        const resizeSize = 2 / scale * dpr
+        const lineColor = '#5491f8'
+  
+        const o = module!.getOperators({
+          size: resizeSize,
+          lineColor,
+          lineWidth,
+        }, {
+          size: 1,
+          lineColor: '',
+          lineWidth: 0,
+        })
+  
+        o.forEach(
+          (p) => {
+            // @ts-ignore
+            this.operationHandlers.add(p)
+          },
+        )
+      }
+    */
+}
