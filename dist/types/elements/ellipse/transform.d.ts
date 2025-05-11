e84e578054a9dcf45e95b91d8552eb83df1fca85
yahone <yahone.chow@gmail.com>
2025-05-11 14:49:06 +0900

    ###

---
+++ dist/types/elements/ellipse/transform.d.ts
@@ -0,0 +1,24 @@
+import { ResizeHandleName } from '~/engine/selection/type';
+import { Point } from '~/type';
+import { RequiredEllipseProps } from '~/elements/ellipse/ellipse';
+declare const transform: ({ downPoint, movePoint, moduleOrigin, rotation, handleName, scale, dpr, altKey, shiftKey, }: {
+    downPoint: {
+        x: number;
+        y: number;
+    };
+    movePoint: {
+        x: number;
+        y: number;
+    };
+    moduleOrigin: RequiredEllipseProps;
+    rotation: number;
+    handleName: ResizeHandleName;
+    scale: number;
+    dpr: number;
+    altKey?: boolean;
+    shiftKey?: boolean;
+}) => Point & {
+    r1: number;
+    r2: number;
+};
+export default transform;
