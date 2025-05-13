ff1ee272b44b86f79ea640171884aee8d68b62db
yahone <yahone.chow@gmail.com>
2025-05-13 15:00:02 +0900

    ###

--- src/elements/rectangle/render.ts
+++ src/elements/rectangle/render.ts
@@ -3,7 +3,7 @@ import RectangleLike from './rectangleLike'
 function render(rect: RectangleLike, ctx: CanvasRenderingContext2D): void {
   let {cx, cy, show, width, height, rotation, opacity, fill, stroke, borderRadius} = rect.toJSON()
   const {enabled: enabledFill, color: fillColor} = fill
-  const {enabled: enabledStroke, color: strokeColor, weight, join, dashed, cap} = stroke
+  const {enabled: enabledStroke, color: strokeColor, weight, join, dashed} = stroke
   // x = Math.round(x)
   // y = Math.round(y)
   // width = Math.round(width)
