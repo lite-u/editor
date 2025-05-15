9a90f5ef62219ca665bb060abb9fe7b5efbc0058
yahone <yahone.chow@gmail.com>
2025-05-15 18:53:47 +0900

    ###

--- src/services/tool/events/keyUp.ts
+++ src/services/tool/events/keyUp.ts
@@ -6,19 +6,11 @@ function handleKeyUp(this: ToolManager, e: KeyboardEvent) {
   const {interaction, toolManager, action} = this.editor
   const {shiftKey, metaKey, ctrlKey, altKey} = e
 
-  console.log(e.code)
   if (e.code === 'Space') {
-
-    console.log(interaction._lastTool)
-    // interaction.spaceKeyDown = false
     if (interaction._lastTool) {
       toolManager.set(interaction._lastTool)
       action.dispatch('switch-tool', interaction._lastTool)
     }
-
-    // cursor.set('selector')
-    // interaction._lastTool = toolManager.currentToolName
-
     e.preventDefault()
   } else {
     interaction._modifier = {...interaction._modifier, shiftKey, metaKey, ctrlKey, altKey}
