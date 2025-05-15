f851b84ccbd132ad4fc10f487c3e4dee1f2b07cf
yahone <yahone.chow@gmail.com>
2025-05-15 21:40:43 +0900

    ###

---
+++ dist/types/elements/lines/elementLineSegment.d.ts
@@ -0,0 +1,24 @@
+import ElementBase, { ElementBaseProps } from '~/elements/base/elementBase';
+import { BasePath } from '~/elements/basePath/basePath';
+import { Point } from '~/type';
+export interface LineSegmentProps extends ElementBaseProps {
+    id: string;
+    layer: number;
+    type: 'lineSegment';
+    points: Point[];
+}
+export type RequiredLineSegmentProps = Required<LineSegmentProps>;
+declare class ElementLineSegment extends ElementBase implements BasePath {
+    readonly id: string;
+    readonly layer: number;
+    readonly type = "lineSegment";
+    private points;
+    private original;
+    constructor({ id, layer, points, ...rest }: LineSegmentProps);
+    translate(dx: number, dy: number): void;
+    scaleFrom(scaleX: number, scaleY: number, anchor: Point): void;
+    protected toJSON(): RequiredLineSegmentProps;
+    toMinimalJSON(): LineSegmentProps;
+    render(): void;
+}
+export default ElementLineSegment;
