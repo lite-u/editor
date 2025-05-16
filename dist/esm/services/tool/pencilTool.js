0b434126ffc812197edeb1a40aaa72f724f6f159
yahone <yahone.chow@gmail.com>
2025-05-16 10:37:31 +0900

    ###

--- dist/esm/services/tool/pencilTool.js
+++ dist/esm/services/tool/pencilTool.js
@@ -1,39 +1,48 @@
-import nid from '~/core/nid';
+import nid from '../../core/nid.js';
 const points = [];
+let _lastPoint = null;
 const pencilTool = {
     cursor: 'crosshair',
     mouseDown() {
         const { creationCanvasContext: ctx } = this.editor.world;
         const { x, y } = this.editor.interaction.mouseWorldCurrent;
-        this.editor.action.dispatch('selection-clear');
+        // this.editor.action.dispatch('selection-clear')
+        points.push({ x, y });
+        _lastPoint = { x, y };
         ctx.save();
         ctx.strokeStyle = '#000';
-        ctx.lineWidth = 100;
+        ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.moveTo(x, y);
-        ctx.lineTo(x + 100, y + 100);
+        ctx.lineTo(x + 1, y + 1);
         ctx.stroke();
         ctx.restore();
     },
     mouseMove() {
-        const { overlayCanvasContext: ctx } = this.editor.world;
+        if (!this.editor.interaction._pointDown)
+            return;
+        const { creationCanvasContext: ctx } = this.editor.world;
         const { x, y } = this.editor.interaction.mouseWorldCurrent;
-        this.editor.action.dispatch('selection-clear');
+        // this.editor.action.dispatch('selection-clear')
+        points.push({ x, y });
         ctx.save();
         ctx.strokeStyle = '#000';
-        ctx.lineWidth = 100;
+        ctx.lineWidth = 1;
         ctx.save();
         ctx.beginPath();
-        ctx.moveTo(x, y);
-        ctx.lineTo(x + 100, y + 100);
+        ctx.moveTo(_lastPoint.x, _lastPoint.y);
+        ctx.lineTo(x, y);
         ctx.stroke();
         ctx.restore();
+        _lastPoint.x = x;
+        _lastPoint.y = y;
     },
     mouseUp() {
         const { elementManager, interaction, action, selection } = this.editor;
         const { x, y } = this.editor.interaction.mouseWorldCurrent;
         const id = 'rectangle-' + nid();
         interaction._ele = null;
+        console.log(points);
         const eleProps = {
             id,
             layer: 0,
