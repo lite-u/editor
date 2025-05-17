0fa9b22638820ff0a62c1b3cda2f0365cd475fa5
yahone <yahone.chow@gmail.com>
2025-05-17 09:38:03 +0900

    ###

--- src/services/tool/zoomInTool.ts
+++ src/services/tool/zoomInTool.ts
@@ -1,9 +1,11 @@
 import ToolManager, {ToolType} from '~/services/tool/toolManager'
 
+let _timer = null
+
 const zoomInTool: ToolType = {
   cursor: 'zoom-in',
   mouseDown(this: ToolManager) {
-    
+
   },
   mouseMove(this: ToolManager) {},
   mouseUp(this: ToolManager) {},
