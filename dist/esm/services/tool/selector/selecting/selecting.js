891e83048e6c4d99d1ae0ad67d3f508a96c976e8
yahone <yahone.chow@gmail.com>
2025-05-18 23:10:08 +0900

    ###

--- dist/esm/services/tool/selector/selecting/selecting.js
+++ dist/esm/services/tool/selector/selecting/selecting.js
@@ -1,13 +1,28 @@
 import { generateBoundingRectFromTwoPoints } from '../../../../core/utils.js';
+let _mouseMoved = false;
 const selecting = {
     cursor: 'default',
     mouseMove() {
-        const { interaction, action, selection, cursor } = this.editor;
-        const { mouseStart, mouseCurrent, _pointDown, _modifier: { shiftKey, metaKey, ctrlKey } } = interaction;
-        if (!_pointDown)
-            return;
+        const { interaction, action, elementManager, selection, cursor } = this.editor;
+        const { mouseStart, mouseCurrent, mouseWorldStart, mouseWorldCurrent, _pointDown, _modifier: { shiftKey, metaKey, ctrlKey }, } = interaction;
+        // if (!_pointDown) return
+        _mouseMoved = true;
         const rect = generateBoundingRectFromTwoPoints(mouseStart, mouseCurrent);
         interaction.updateSelectionBox(rect);
+        const outer = generateBoundingRectFromTwoPoints(mouseWorldStart, mouseWorldCurrent);
+        const _selecting = new Set();
+        const modifyKey = ctrlKey || metaKey || shiftKey;
+        elementManager.all.forEach((ele) => {
+            const inner = ele.getBoundingRect();
+            if (inner.left >= outer.left &&
+                inner.right <= outer.right &&
+                inner.top >= outer.top &&
+                inner.bottom <= outer.bottom) {
+                _selecting.add(ele.id);
+            }
+        });
+        console.log(_selecting);
+        // const selectingChanged = !areSetsEqual(_selectingElements, _selecting)
     },
     mouseUp() {
         /*const {shiftKey, metaKey, ctrlKey} = this.editor.interaction._modifier
@@ -15,6 +30,10 @@ const selecting = {
         interaction.hideSelectionBox()*/
         this.editor.interaction.hideSelectionBox();
         // this.tool = selector
+        if (!_mouseMoved) {
+            this.editor.action.dispatch('selection-clear');
+        }
+        _mouseMoved = false;
     },
 };
 export default selecting;
--- src/services/tool/selector/selecting/selecting.ts
+++ src/services/tool/selector/selecting/selecting.ts
@@ -1,20 +1,45 @@
 import ToolManager, {SubToolType} from '~/services/tool/toolManager'
 import {generateBoundingRectFromTwoPoints} from '~/core/utils'
-import selector from '~/services/tool/selector/selector'
+import {BoundingRect, UID} from '~/type'
 
+let _mouseMoved = false
 const selecting: SubToolType = {
   cursor: 'default',
   mouseMove(this: ToolManager,) {
-    const {interaction, action, selection, cursor} = this.editor
-    const {mouseStart, mouseCurrent, _pointDown, _modifier: {shiftKey, metaKey, ctrlKey}} = interaction
-    if (!_pointDown) return
-
-    const rect = generateBoundingRectFromTwoPoints(
+    const {interaction, action, elementManager, selection, cursor} = this.editor
+    const {
       mouseStart,
       mouseCurrent,
-    )
-
+      mouseWorldStart,
+      mouseWorldCurrent,
+      _pointDown,
+      _modifier: {shiftKey, metaKey, ctrlKey},
+    } = interaction
+    // if (!_pointDown) return
+    _mouseMoved = true
+    const rect = generateBoundingRectFromTwoPoints(mouseStart, mouseCurrent)
     interaction.updateSelectionBox(rect)
+
+    const outer: BoundingRect = generateBoundingRectFromTwoPoints(mouseWorldStart, mouseWorldCurrent)
+    const _selecting: Set<UID> = new Set()
+    const modifyKey = ctrlKey || metaKey || shiftKey
+
+    elementManager.all.forEach((ele) => {
+      const inner = ele.getBoundingRect()
+
+      if (
+        inner.left >= outer.left &&
+        inner.right <= outer.right &&
+        inner.top >= outer.top &&
+        inner.bottom <= outer.bottom
+      ) {
+        _selecting.add(ele.id)
+      }
+    })
+
+    console.log(_selecting)
+    // const selectingChanged = !areSetsEqual(_selectingElements, _selecting)
+
   },
   mouseUp(this: ToolManager) {
     /*const {shiftKey, metaKey, ctrlKey} = this.editor.interaction._modifier
@@ -22,6 +47,12 @@ const selecting: SubToolType = {
     interaction.hideSelectionBox()*/
     this.editor.interaction.hideSelectionBox()
     // this.tool = selector
+
+    if (!_mouseMoved) {
+      this.editor.action.dispatch('selection-clear')
+    }
+
+    _mouseMoved = false
   },
 }
 
