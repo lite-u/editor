fbe5cf028620c59b64a43a2bf5f96eaf258df550
yahone <yahone.chow@gmail.com>
2025-05-20 15:03:57 +0900

    original and current

--- dist/esm/lib/lib.js
+++ dist/esm/lib/lib.js
@@ -1,7 +1,7 @@
-import Rectangle from '~/elements/rectangle/rectangle';
-import { DEFAULT_STROKE } from '~/elements/defaultProps';
-import Ellipse from '~/elements/ellipse/ellipse';
-import { rotatePointAroundPoint } from '~/core/geometry';
+import Rectangle from '../elements/rectangle/rectangle.js';
+import { DEFAULT_STROKE } from '../elements/defaultProps.js';
+import Ellipse from '../elements/ellipse/ellipse.js';
+import { rotatePointAroundPoint } from '../core/geometry.js';
 /** Convert screen (mouse) coordinates to canvas coordinates */
 export function screenToWorld(point, offset, scale, dpr) {
     return {
