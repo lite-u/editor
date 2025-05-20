3fe485773e67a9fc10a2b5e5e4c127fbb2797466
yahone <yahone.chow@gmail.com>
2025-05-20 14:34:52 +0900

    original and current

--- dist/esm/services/tool/selector/resizing/resizing.js
+++ dist/esm/services/tool/selector/resizing/resizing.js
@@ -1,8 +1,19 @@
+import resizeFunc from '../../resize/resizeFunc.js';
 const resizing = {
-    cursor: 'default',
+    // cursor: 'default',
     mouseMove() {
+        console.log(111);
+        // interaction._resizingElements = elementManager.getElementsByIdSet(selection.values)
+        const { interaction, elementManager, action, selection, cursor } = this.editor;
+        resizeFunc.call(this, elementManager.getElementsByIdSet(selection.values), interaction._resizingData.placement);
+        this.editor.action.dispatch('element-updated');
+        // this.subTool.mouseMove.call(this)
     },
     mouseUp() {
+        if (!this.subTool)
+            return;
+        // this.subTool.mouseUp.call(this)
+        this.subTool = null;
     },
 };
 export default resizing;
