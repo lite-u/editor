4829e2eb867e62f0726a8376da3591aad56525da
yahone <yahone.chow@gmail.com>
2025-05-20 22:44:57 +0900

    original and current

--- dist/esm/services/tool/selector/rotating/rotating.js
+++ dist/esm/services/tool/selector/rotating/rotating.js
@@ -4,27 +4,25 @@ const rotating = {
     mouseMove() {
         const { interaction, elementManager, action, selection, cursor } = this.editor;
         const elements = elementManager.getElementsByIdSet(selection.values);
-        const { _rotateData, mouseWorldCurrent } = interaction;
+        const { _rotateData, mouseWorldCurrent, mouseWorldStart } = interaction;
         console.log(_rotateData);
         if (!_rotateData)
             return;
-        const { targetPoint } = _rotateData;
-        const rotate = getRotateAngle(targetPoint, mouseWorldCurrent);
+        const { startRotation, targetPoint } = _rotateData;
+        const mouseStartRotation = getRotateAngle(targetPoint, mouseWorldStart);
+        const mouseCurrentRotation = getRotateAngle(targetPoint, mouseWorldCurrent);
         /*  console.log(
             center, mouseWorldCurrent,
           )*/
-        console.log(rotate);
-        /* const rects = elements.map((ele: ElementInstance) => {
-           return ele.getBoundingRect()
-         })*/
-        // const rect = getBoundingRectFromBoundingRects(rects)
+        const rotationDiff = mouseCurrentRotation - mouseStartRotation;
+        console.log(rotationDiff);
         // const center = {x: rect.cx, y: rect.cy}
-        interaction._outlineElement?.rotateFrom(rotate, targetPoint);
+        interaction._outlineElement?.rotateFrom(rotationDiff, targetPoint);
         interaction._manipulationElements.forEach(ele => {
-            ele.rotateFrom(rotate, targetPoint);
+            ele.rotateFrom(rotationDiff, targetPoint);
         });
         elements.forEach(ele => {
-            ele.rotateFrom(rotate, targetPoint);
+            ele.rotateFrom(rotationDiff, targetPoint);
         });
         // resizeFunc.call(this, elementManager.getElementsByIdSet(selection.values), interaction.startRotation)
         // dispatch('render-overlay')
