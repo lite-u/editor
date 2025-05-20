d37c8a16fb5ca9773d4fdaf4151a45710de25881
yahone <yahone.chow@gmail.com>
2025-05-20 13:16:41 +0900

    original and current

--- src/services/tool/detectTool.ts
+++ src/services/tool/detectTool.ts
@@ -1,7 +1,5 @@
 import ToolManager from '~/services/tool/toolManager'
-import {Point} from '~/type'
 import {isPointNearStroke2} from '~/services/tool/helper'
-import {isPointNear} from '~/core/geometry'
 
 function detectTool(this: ToolManager) {
   const {interaction, world, visible} = this.editor
@@ -12,45 +10,24 @@ function detectTool(this: ToolManager) {
     y: y * dpr,
   }
   const arr = visible.values
-  let _ele = null
-  let _snappedPoint = null
+  // let _ele = null
+  interaction._hoveredElement = null
+
   for (let i = arr.length - 1; i >= 0; i--) {
     const ele = arr[i]
     const path = ele.path2D
-    // console.log(interaction._hoveredElement)
     if (!ele.show || ele.opacity <= 0 || interaction._draggingElements.includes(ele)) continue
 
-    const points: Point[] = ele.getPoints
-    // const border = ctx.isPointInStroke(path, viewPoint.x, viewPoint.y)
-    // const onBorder = isPointNearStroke(ctx, path, viewPoint, 2, .1)
     const lineWidth = 1 * dpr / scale
     const isNearStroke = isPointNearStroke2(ctx, path, viewPoint, 0, lineWidth)
-    // const isOn = isPointNearStroke(ctx, path, viewPoint, 2, 1)
     const inside = ctx.isPointInPath(path, viewPoint.x, viewPoint.y)
-    const point = points.find(p => isPointNear(p, viewPoint))
-    if (point) {
-      // _snapped = true
-      _ele = ele
-      _snappedPoint = {type: 'anchor', ...point}
-      break
-    } else if (isNearStroke) {
 
-      _snappedPoint = {
-        type: 'path',
-        ...world.getWorldPointByViewportPoint(
-          viewPoint.x / dpr,
-          viewPoint.y / dpr,
-        ),
-      }
-      break
-    } else if (inside) {
-      if (ele.fill.enabled) {
-        _ele = ele
-      }
+    if (isNearStroke) {
+      interaction._hoveredElement = ele
+    } else if (inside && ele.fill.enabled) {
+      interaction._hoveredElement = ele
     }
   }
-
-  interaction._hoveredElement = _ele
 }
 
 export default detectTool
