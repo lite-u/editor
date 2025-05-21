57854f71c83574dd1377e97397b7c35091834d64
yahone <yahone.chow@gmail.com>
2025-05-21 17:00:19 +0900

    merge shape into base

--- src/services/tool/selector/selector.ts
+++ src/services/tool/selector/selector.ts
@@ -30,7 +30,7 @@ const selector: ToolType = {
       const center = getBoundingRectFromBoundingRects(rects)
       const {cx: x, cy: y} = center
 
-      interaction._rotateData = {startRotation: interaction._outlineElement.rotation, targetPoint: {x, y}}
+      interaction._rotateData = {startRotation: interaction._hoveredElement.rotation, targetPoint: {x, y}}
       this.subTool = rotating
     } else if (dragMode) {
       this.subTool = dragging
