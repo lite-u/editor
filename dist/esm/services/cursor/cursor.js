23ae53fb8d8f1d95b624b42f46d97cb56e18d791
yahone <yahone.chow@gmail.com>
2025-05-21 20:06:14 +0900

    merge shape into base

--- dist/esm/services/cursor/cursor.js
+++ dist/esm/services/cursor/cursor.js
@@ -17,6 +17,7 @@ const CURSORS = {
     // grabbing: `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M18 11.5V9a2 2 0 0 0-2-2a2 2 0 0 0-2 2v1.4"></path><path d="M14 10V8a2 2 0 0 0-2-2a2 2 0 0 0-2 2v2"></path><path d="M10 9.9V9a2 2 0 0 0-2-2a2 2 0 0 0-2 2v5"></path><path d="M6 14a2 2 0 0 0-2-2a2 2 0 0 0-2 2"></path><path d="M18 11a2 2 0 1 1 4 0v3a8 8 0 0 1-8 8h-4a8 8 0 0 1-8-8 2 2 0 1 1 4 0"></path></svg>`,
     rotate: `<?xml version="1.0" encoding="UTF-8"?><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.99 18.93"><path d="M15.57,16.04c.48-.57,1-1.1,1.5-1.65.35-.38.54-.85,1.15-.82.72.04,1.01.78.55,1.34-1.07,1.3-2.37,2.5-3.44,3.82-.69.48-1.09,0-1.56-.48-.91-.95-1.92-2.07-2.78-3.07-.3-.35-.58-.72-.33-1.19.39-.73,1.08-.39,1.53.07.52.55,1.01,1.17,1.51,1.74.07.08.17.19.26.22,0-.81.02-1.64-.08-2.44-.51-4.07-3.48-7.36-7.47-8.32-1.12-.27-2.22-.25-3.37-.24.15.2.34.39.52.56.51.47,1.2.95,1.66,1.44.69.73-.3,1.84-1.1,1.19-.91-.74-1.83-1.68-2.7-2.48-.38-.35-1.06-.81-1.29-1.26-.21-.4-.1-.54.14-.89C1.59,2.57,2.79,1.18,4.11.18c.76-.58,1.72.38,1.13,1.14l-2.3,2.09c.76.04,1.52-.01,2.28.06,5.2.47,9.42,4.43,10.22,9.59.15.99.1,1.98.14,2.98Z"/></svg>`,
 };
+const SVG_SIZE = 16;
 class Cursor {
     domRef;
     editor;
@@ -26,8 +27,8 @@ class Cursor {
         this.editor = editor;
         this.domRef = createWith('div', 'cursor', editor.id, {
             pointerEvents: 'none',
-            width: '20px',
-            height: '20px',
+            width: SVG_SIZE + 'px',
+            height: SVG_SIZE + 'px',
             position: 'fixed',
             top: '0',
             left: '0',
@@ -55,8 +56,7 @@ class Cursor {
         // console.log('set cursor', p)
         // this.domRef.style.left = `${p.x}px`
         // this.domRef.style.top = `${p.y}px`
-        const len = 20;
-        this.domRef.style.translate = `${p.x - len / 2}px ${p.y - len / 2}px`;
+        this.domRef.style.translate = `${p.x - SVG_SIZE / 2}px ${p.y - SVG_SIZE / 2}px`;
     }
     rotate(rotation) {
         // const size = 24
