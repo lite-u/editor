842fb8b722d25b30dcd903ebe4d5f2b72b953ec3
yahone <yahone.chow@gmail.com>
2025-05-25 00:58:21 +0900

    ###

--- src/services/tool/pencil/pencilTool.ts
+++ src/services/tool/pencil/pencilTool.ts
@@ -7,26 +7,26 @@ const _drawingPoints: Point[] = []
 let _lastPoint = null
 const pencilTool: ToolType = {
   cursor: 'crosshair',
-  mouseDown(this: ToolManager) {
-    const {creationCanvasContext: ctx, scale, dpr} = this.editor.world
-    const point = {...this.editor.interaction.mouseWorldCurrent}
+  mouseDown:function() {
+    const {creationCanvasContext: ctx, scale, dpr} = this.world
+    const point = {...this.interaction.mouseWorldCurrent}
 
-    this.editor.action.dispatch('clear-creation')
+    this.action.dispatch('clear-creation')
     _drawingPoints.push(point)
     _lastPoint = {...point}
     drawLine(ctx, _lastPoint, point, 1 / scale * dpr)
   },
-  mouseMove(this: ToolManager) {
-    if (!this.editor.interaction._pointDown) return
-    const point = {...this.editor.interaction.mouseWorldCurrent}
-    const {creationCanvasContext: ctx, scale, dpr} = this.editor.world
+  mouseMove:function() {
+    if (!this.interaction._pointDown) return
+    const point = {...this.interaction.mouseWorldCurrent}
+    const {creationCanvasContext: ctx, scale, dpr} = this.world
 
     _drawingPoints.push(point)
     drawLine(ctx, _lastPoint!, point, 1 / scale * dpr)
     _lastPoint = point
   },
-  mouseUp(this: ToolManager) {
-    const {interaction, action} = this.editor
+  mouseUp:function() {
+    const {interaction, action} = this
     const {center, points, closed} = convertPointsToBezierPoints(_drawingPoints)
 
     const eleProps: PropsWithoutIdentifiers<'path'> = {
