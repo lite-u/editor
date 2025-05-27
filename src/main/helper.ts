9192d3b110b5b0352d9e92650475ec7b2f58b317
yahone <yahone.chow@gmail.com>
2025-05-27 17:43:48 +0900

    ###

--- src/main/helper.ts
+++ src/main/helper.ts
@@ -132,16 +132,15 @@ export function getSelectedBoundingElement(this: Editor): ElementRectangle {
   const ratio = dpr / scale
   const pointLen = 2 / ratio
   const idSet = selection.values
-  // const visibleElements = mainHost.visibleElements
+
   const selectedElements = mainHost.getVisibleElementsByIdSet(idSet)
-  // console.log(idSet)
+
   selectedElements.forEach((ele: ElementInstance) => {
     rotations.push(ele.rotation)
     rectsWithRotation.push(ele.getBoundingRect())
     rectsWithoutRotation.push(ele.getBoundingRect(true))
   })
-  // console.log(selectedElements)
-  // selectedOutlineElement
+
   const sameRotation = rotations.every(val => val === rotations[0])
   let applyRotation = sameRotation ? rotations[0] : 0
   let rect: { cx: number, cy: number, width: number, height: number }
