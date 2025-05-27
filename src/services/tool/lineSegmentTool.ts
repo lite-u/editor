cb192c4f57106d1c9bdf2e8c8ee5eefc6a71b598
yahone <yahone.chow@gmail.com>
2025-05-27 20:11:08 +0900

    ###

--- src/services/tool/lineSegmentTool.ts
+++ src/services/tool/lineSegmentTool.ts
@@ -5,7 +5,7 @@ import ElementLineSegment from '~/elements/lines/lineSegment'
 const lineSegmentTool: ToolType = {
   cursor: 'crosshair',
   mouseDown(this: ToolManager) {
-    const {mainHost, interaction, world} = this.editor
+    const {mainHost, action, cursor, overlayHost, interaction, world} = this.editor
     const {x, y} = interaction.mouseWorldCurrent
     let lineLen = 1
     const cx = x + lineLen / 2
@@ -14,24 +14,28 @@ const lineSegmentTool: ToolType = {
     const ele = ElementLineSegment.create('lineSegment-creating', cx, cy, x - cx + lineLen, y - cy + lineLen)
     // const ele: ElementRectangle = mainHost.create(eleProps)
 
-    ele.render(world.creationCanvasContext)
+    cursor.lock()
+    action.dispatch('rerender-overlay')
+    ele.render(overlayHost.ctx)
     interaction._ele = ele
   },
   mouseMove(this: ToolManager) {
-    const {action, interaction, world} = this.editor
+    const {action, interaction, cursor, overlayHost, world} = this.editor
 
     if (!interaction._ele) return
 
     action.dispatch('clear-creation')
 
     resizeFunc.call(this, [interaction._ele], 'br')
-    interaction._ele.render(world.creationCanvasContext)
+    action.dispatch('rerender-overlay')
+    interaction._ele.render(overlayHost.ctx)
   },
   mouseUp(this: ToolManager) {
-    const {action, interaction} = this.editor
+    const {action, interaction, cursor} = this.editor
 
     const eleProps = interaction._ele.toMinimalJSON()
 
+    cursor.unlock()
     action.dispatch('element-add', [eleProps])
     interaction._ele = null!
   },
