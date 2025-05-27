5ea877cb46658423337b7391d4deef9cfdbe7f73
yahone <yahone.chow@gmail.com>
2025-05-27 20:01:24 +0900

    ###

--- dist/esm/services/tool/lineSegmentTool.js
+++ dist/esm/services/tool/lineSegmentTool.js
@@ -1,38 +1,43 @@
 import resizeFunc from './resize/resizeFunc.js';
+import ElementLineSegment from '../../elements/lines/lineSegment.js';
 const lineSegmentTool = {
     cursor: 'crosshair',
     mouseDown() {
-        const { mainHost, interaction, world } = this;
-        const { x, y } = this.interaction.mouseWorldCurrent;
-        let initialLineLen = 1;
-        const cx = x + initialLineLen / 2;
-        const cy = y + initialLineLen / 2;
+        const { mainHost, interaction, world } = this.editor;
+        const { x, y } = interaction.mouseWorldCurrent;
+        let lineLen = 1;
+        const cx = x + lineLen / 2;
+        const cy = y + lineLen / 2;
         // const startPoint = {x, y}
         // const endPoint = {x: x + initialLineLen, y: y + initialLineLen}
-        const eleProps = {
-            type: 'lineSegment',
-            cx,
-            cy,
-            points: [
-                { id: 'start', x: x - cx, y: y - cy },
-                { id: 'end', x: x - cx + initialLineLen, y: y - cy + initialLineLen },
-            ],
-        };
-        const ele = mainHost.create(eleProps);
+        /* const eleProps: PropsWithoutIdentifiers<'lineSegment'> = {
+           type: 'lineSegment',
+           cx,
+           cy,
+           points: [
+             {id: 'start', x: x - cx, y: y - cy},
+             {id: 'end', x: x - cx + lineLen, y: y - cy + lineLen},
+           ],
+         }
+     */
+        const ele = ElementLineSegment.create('lineSegment-creating', x - cx, y - cy, x - cx + lineLen, y - cy + lineLen);
+        // const ele: ElementRectangle = mainHost.create(eleProps)
         ele.render(world.creationCanvasContext);
         interaction._ele = ele;
     },
     mouseMove() {
-        if (!this.interaction._ele)
+        const { action, interaction, world } = this.editor;
+        if (!interaction._ele)
             return;
-        this.action.dispatch('clear-creation');
-        resizeFunc.call(this, [this.interaction._ele], 'br');
-        this.interaction._ele.render(this.world.creationCanvasContext);
+        action.dispatch('clear-creation');
+        resizeFunc.call(this, [interaction._ele], 'br');
+        interaction._ele.render(world.creationCanvasContext);
     },
     mouseUp() {
-        const eleProps = this.interaction._ele.toMinimalJSON();
-        this.action.dispatch('element-add', [eleProps]);
-        this.interaction._ele = null;
+        const { action, interaction } = this.editor;
+        const eleProps = interaction._ele.toMinimalJSON();
+        action.dispatch('element-add', [eleProps]);
+        interaction._ele = null;
     },
 };
 export default lineSegmentTool;
--- src/services/tool/selector/resizing/resizing.ts
+++ src/services/tool/selector/resizing/resizing.ts
@@ -4,25 +4,23 @@ import resizeFunc from '~/services/tool/resize/resizeFunc'
 const resizing: SubToolType = {
   // cursor: 'default',
   mouseMove(this: ToolManager) {
-    // console.log(111)
-    // interaction._resizingElements = elementManager.getElementsByIdSet(selection.values)
-    const {interaction, mainHost, action, selection, cursor} = this
+    const {interaction, mainHost, action, selection, cursor} = this.editor
 
     resizeFunc.call(this, mainHost.getElementsByIdSet(selection.values), interaction._resizingData.placement)
 
-    this.action.dispatch('element-updated')
+    action.dispatch('element-updated')
 
-/*
-    interaction._outlineElement?.rotateFrom(rotationDiff, targetPoint)
-    interaction._manipulationElements.forEach(ele => {
-      ele.rotateFrom(rotationDiff, targetPoint)
-    })
-    elements.forEach(ele => {
-      ele.rotateFrom(rotationDiff, targetPoint)
-    })
+    /*
+        interaction._outlineElement?.rotateFrom(rotationDiff, targetPoint)
+        interaction._manipulationElements.forEach(ele => {
+          ele.rotateFrom(rotationDiff, targetPoint)
+        })
+        elements.forEach(ele => {
+          ele.rotateFrom(rotationDiff, targetPoint)
+        })
 
-    this.action.dispatch('render-overlay')
-    this.action.dispatch('render-main-host')*/
+        this.action.dispatch('render-overlay')
+        this.action.dispatch('render-main-host')*/
     // this.subTool.mouseMove.call(this)
   },
   mouseUp(this: ToolManager) {
